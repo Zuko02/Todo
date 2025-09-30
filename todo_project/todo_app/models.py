5d7daabc2054e07f77089d63883219bf0254024f
F4thih <zuko.audit@outlook.com>
2025-09-30 22:47:55 +0530

    added description as a model
    added model description
    did migrations

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
